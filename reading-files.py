4a4e75f84cdcd99b8c331ddd1988d97a8fe0866b
santosh <santoshtechguy@gmail.com>
2023-11-06 11:37:20 +0200

    adding new code to find information about system

---
+++ reading-files.py
@@ -0,0 +1,5 @@
+
+with open("Life Expectancy at Birth.csv", 'r') as file:
+    for eachline in file:
+        print(eachline)
+
---
+++ readline-example.py
@@ -0,0 +1,8 @@
+
+with open("Life Expectancy at Birth.csv", 'r') as file:
+    lines=file.readlines()
+
+print(lines)
+
+
+
